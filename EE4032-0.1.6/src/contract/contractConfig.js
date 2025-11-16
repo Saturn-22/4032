5fecf5b111176ca0889dc50099715d1fe51dc03a
Kang <19875682981@163.com>
2025-11-16 19:47:42 +0800

    1947

--- EE4032-0.1.6/src/contract/contractConfig.js
+++ EE4032-0.1.6/src/contract/contractConfig.js
@@ -2,8 +2,8 @@
 // export const GOMOKU_ADDRESS = "0x77d58dFCe464ec63D9296C92E33BA353e0C2C5d4";
 
 
-export const USER_VAULT_ADDRESS = "0x8a473EFb0e31cafaaCA97269A0EC0C240C01FC0F";
-//export const USER_VAULT_ADDRESS = "0x0d57b01f7F1dd4f8B5f42958FDaBD21a31E89666";
+//export const USER_VAULT_ADDRESS = "0x8a473EFb0e31cafaaCA97269A0EC0C240C01FC0F";
+export const USER_VAULT_ADDRESS = "0x0d57b01f7F1dd4f8B5f42958FDaBD21a31E89666";
 export const GOMOKU_ADDRESS = "0x9CF247E6Ee8B87feCD0B6e52975653c7D59729d0";
 // Blackjack Settlement Contract Address
 // TODO: Update this after deploying the Blackjack contract
